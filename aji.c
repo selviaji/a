5a8db45c558936463c91ff624421346a0e37e369
selviaji <ajithngc@gmail.com>
2017-04-18 15:54:33 +0530

    Add files via upload

---
+++ aji.c
@@ -0,0 +1,12 @@
+class Simple{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+ int a=sc.nextInt();
+ if(a==0)
+ System.out.println("zero");
+ if(a>0)
+ System.out.println("postive");
+ else
+ System.out.println("negative");
+ }
+ }
